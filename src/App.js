0cf7a1db933c2517c2179273dd9f082f41015082
Sofia Goyal <sofia.goyal@bold.com>
2020-02-06 11:59:15 +0530

    Shop page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+import {Switch , Route} from 'react-router-dom';
+
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './collection-preview.style.scss';
+
+import CollectionItem from '../collection-items/collection-items.component'
+
+const CollectionPreview = ({title, items}) => (
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <div className='preview'>
+            {items
+            .filter((item, idx) => idx < 4)
+            .map(({id, ...otheritemprops}) => (
+                <CollectionItem key={id} {...otheritemprops} />
+            ))}
+        </div>
+    </div>
+)
+
+export default CollectionPreview;
