de8d2ba8d3b7effdbb8afeafe81db21d797ed311
Sofia Goyal <sofia.goyal@bold.com>
2020-04-14 17:19:10 +0530

    toggle cart functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,11 +4,12 @@ import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../../src/assets/crown.svg';
 
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.style.scss';
 import { auth } from '../../firebase/firebase.util';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser,hidden}) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
@@ -24,11 +25,16 @@ const Header = ({currentUser}) => (
             }
             <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropDown />
+        }
+        
     </div>
 );
 
-const mapStateToProps = state => ({
-   currentUser : state.user.currentUser  //this state is the entire state of redux store, we can refer it as root-reducer
+const mapStateToProps = ({user : {currentUser}, cart : {hidden}}) => ({
+   currentUser,  //this state is the entire state of redux store, we can refer it as root-reducer
+   hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 //this is imported to make it root-redducer so that it can handle multiple reducers
 
 import userReducer from './user/user-reducer';
+import cartReducer from './cart/cart-reducer';
 
 export default combineReducers({
-    user : userReducer
+    user : userReducer,
+    cart : cartReducer
 });
 
 //this combine reducer is basically JSON of various reducers where the key represents eaxh user
